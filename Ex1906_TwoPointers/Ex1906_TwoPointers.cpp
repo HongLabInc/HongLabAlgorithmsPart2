8a217eca66349bf969e2d23255630a60f277a682
HongLabInc <95195816+HongLabInc@users.noreply.github.com>
2024-09-20 20:24:28 -0700

    Update comments.

--- Ex1906_TwoPointers/Ex1906_TwoPointers.cpp
+++ Ex1906_TwoPointers/Ex1906_TwoPointers.cpp
@@ -15,12 +15,14 @@ int main()
 
 	sort(arr.begin(), arr.end()); // 정렬을 하고 시작합니다.
 
-	int i = 0;
-	int j = int(arr.size()) - 1;
+	int i = 0;                    // 한 인덱스는 맨 앞에서 시작합니다.
+	int j = int(arr.size()) - 1;  // 다른 인덱스는 맨 뒤에서 시작합니다.
+
+	// 인덱스도 특정 메모리 위치를 가리킨다는 관점에서 포인터로 볼 수 있습니다.
 
 	while (i < j)
 	{
-		cout << i << " " << j << endl;
+		// cout << i << " " << j << endl;
 
 		if (arr[i] + arr[j] == target)
 		{
@@ -31,6 +33,8 @@ int main()
 		//	TODO:
 		//else // if (arr[i] + arr[j] > target)
 		//	TODO:
+
+		break; // 무한 반복 방지 (TODO: 삭제하세요)
 	}
 
 	return 0;
