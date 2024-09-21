4598b6a1c88914f029938697b01eb5c370cc1911
HongLabInc <95195816+HongLabInc@users.noreply.github.com>
2024-09-20 21:56:22 -0700

    Adding one more test case to Ex1902.

--- Ex1902_NaiveStringMatching/Ex1902_NaiveStringMatching.cpp
+++ Ex1902_NaiveStringMatching/Ex1902_NaiveStringMatching.cpp
@@ -5,8 +5,8 @@ using namespace std;
 // CLRS 4th 32.1
 void NaiveStringMatcher(string& pat, string& txt)
 {
-	int M = pat.size();
-	int N = txt.size();
+	int M = int(pat.size());
+	int N = int(txt.size());
 
 	cout << txt << " " << pat << endl;
 	for (int i = 0; i < N; i++)
@@ -29,6 +29,10 @@ int main()
 	string txt2 = "AABAACAADAABAABA";
 	string pat2 = "AABA";
 	NaiveStringMatcher(pat2, txt2);
+
+	string txt3 = "ababababcababababab";
+	string pat3 = "abab";
+	NaiveStringMatcher(pat3, txt3);
 
 	return 0;
 }
