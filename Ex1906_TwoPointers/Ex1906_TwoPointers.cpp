168ca9a897854b8cb3ccf92c75e80a992b64e7d3
HongLabInc <95195816+HongLabInc@users.noreply.github.com>
2024-09-20 20:21:45 -0700

    Ex1906_TwoPointers

---
+++ Ex1906_TwoPointers/Ex1906_TwoPointers.cpp
@@ -0,0 +1,37 @@
+﻿#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+// 참고: 
+// https://www.geeksforgeeks.org/two-pointers-technique/
+// https://leetcode.com/problems/two-sum/description/
+
+int main()
+{
+	vector<int> arr = { 1, 2, 5, 7, 8, 9, 10, 11, 12 };
+
+	int target = 16;
+
+	sort(arr.begin(), arr.end()); // 정렬을 하고 시작합니다.
+
+	int i = 0;
+	int j = int(arr.size()) - 1;
+
+	while (i < j)
+	{
+		cout << i << " " << j << endl;
+
+		if (arr[i] + arr[j] == target)
+		{
+			cout << arr[i] << " + " << arr[j] << " = " << target << endl;
+			return 0;
+		}
+		//else if (arr[i] + arr[j] < target)
+		//	TODO:
+		//else // if (arr[i] + arr[j] > target)
+		//	TODO:
+	}
+
+	return 0;
+}
